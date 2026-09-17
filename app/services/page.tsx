c4fdbd6c91114148e97e25954d21f2c3022a8faf
krishna2062 <kushalpandey1100@gmail.com>
2026-09-17 08:02:10 +0545

    Update portfolio website

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -12,8 +12,8 @@ const Services = () => {
     <div className="min-h-screen h-auto bg-primary/30 pt-24 lg:pt-36 pb-28 lg:pb-0 flex items-center relative overflow-y-auto overflow-x-hidden">
       <Circles />
       <div className="container mx-auto px-4 sm:px-8 lg:px-16">
-        <div className="flex flex-col xl:flex-row gap-x-8 items-center justify-center min-h-screen">
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0 mt-8 lg:mt-0">
+        <div className="flex flex-col xl:flex-row gap-x-8 gap-y-4 xl:gap-y-0 items-center justify-center w-full h-full xl:min-h-screen">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-2 xl:mb-0">
             <motion.h2
               variants={fadeIn("up", 0.2)}
               initial="hidden"
@@ -28,7 +28,7 @@ const Services = () => {
               initial="hidden"
               animate="show"
               exit="hidden"
-              className="mb-4 max-w-100 mx-auto lg:mx-0 text-sm sm:text-base text-white/70">
+              className="max-w-100 mx-auto lg:mx-0 text-sm sm:text-base text-white/70">
               I build reliable, scalable, and user-focused digital solutions that help businesses turn ideas into practical software.
             </motion.p>
           </div>
@@ -38,7 +38,7 @@ const Services = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="w-full xl:max-w-[65%] mb-8 lg:mb-0"
+            className="w-full xl:max-w-[65%] mt-2 xl:mt-0"
           >
             <ServiceSlider />
           </motion.div>
